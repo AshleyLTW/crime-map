913c2bec4cacdd7adabe12f1609e62b6b249a920
Ashley Wong <ashleyltwong@gmail.com>
2019-04-07 21:11:27 -0400

    write header

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # crime-map
 Scraping HUPD crime reports and rendering visually
+
+
+### Tested modules: 
+
+- ~~Textract~~
+- ~~PyPDF2~~
+- ~~Tabula~~ (works well enough, will take some RegEx finangling, but is usable
+- Camelot (works very nicely, but completely omits time and address line other than first line) <- take that back. I think it works perfectly
--- extract.py
+++ extract.py
@@ -1,13 +1,52 @@
+from typing import List
+
 import camelot
+import csv
+from os import listdir
+from fnmatch import fnmatch
+import re
 
-tables = camelot.read_pdf('test.pdf', pages='1-end')
-
-# Export all tables at once
+# # Convert pdf to csv
+# tables = camelot.read_pdf('test.pdf', pages='1-end')
+#
+# # Export all tables at once
 # tables.export('output.csv', f='csv')
+#
+# # Export one table at a time
+# # tables[1].to_csv('output.csv')
 
-# Export one table at a time
-tables[1].to_csv('output.csv')
+# Clean csv
+fileList = []
+for file in listdir('.'):
+    if fnmatch(file, '*.csv'):
+        fileList.append(file)
 
-# Things to consider:
-# Export all of the tables at once in a zip file and then unzip
-# Export them one by one and then manipulate them?
+with open('output.csv', 'a', newline='') as outputFile:
+    fieldnames = ['Date/Time Reported', 'Incident Type', 'Date/Time Occurred', 'Location', 'Disposition', 'Description']
+    # writer = csv.DictWriter(outputFile, fieldnames=fieldnames)
+    writer = csv.writer(outputFile)
+    # writer.writeheader()
+    writer.writerow(fieldnames)
+
+    # for file in fileList:
+    #     with open(file, newline='') as csvFile:
+    #         reader = csv.DictReader(csvFile, fieldnames=fieldnames)
+    #         # Ignore first row (faulty headers)
+    #         next(reader)
+    #         # Check if second row is description or new incident
+    #         row2 = next(reader)
+    #         if re.search(r'\d/\d/\d\d', row2['Date/Time Reported']):
+    #             mod = 0
+    #         else:
+    #             mod = 1
+    #         # Iterate through pairs of rows and condense them into single row
+    #         for row in reader:
+    #             row_write = []
+    #             if reader.line_num % 2 == mod:
+    #                 # data line
+    #                 row_write = [row['Date/Time Reported'], row['Incident Type'],
+    #                              row['Date/Time Occurred'], row['Location'], row['Disposition']]
+    #             else:
+    #                 row_write.append(row['Date/Time Reported'])
+    #                 writer.writerow(row_write)
+    #             # print(write_row)
