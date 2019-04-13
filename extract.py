9cab4ff01b3599bfb7d7fc7038b07a60d14e43f1
Ashley Wong <ashleyltwong@gmail.com>
2019-04-13 13:37:26 -0400

    read rows into db

--- extract.py
+++ extract.py
@@ -12,15 +12,7 @@ db = firestore.client()
 
 crimes_ref = db.collection(u"crime-logs")
 
-
-report = {"address": "test_address", "description": "nothing happened", "occurred": "1/1/1999 9:41pm",
-            "reported": "13/3/1978 3:50pm", "status": "CLOSED", "type": "test"}
-db.collection(u'crime-logs').add(report)
-
-
-geolocater = Nominatim(user_agent = "harvardopendataproject")
-
-
+# geolocater = Nominatim(user_agent = "harvardopendataproject")
 
 # Extract and clean data
 tables = camelot.read_pdf("test.pdf", pages = "1-end")
@@ -34,14 +26,13 @@ for table in tables:
         if row_num % 2 == 0:
             clean_row = [x for x in row if len(x) > 0]
             clean_row[3] = clean_row[3].replace("\n", " ")
-            location = geolocater.geocode(clean_row[3])
-            row_dict = {"reported": clean_row[0], "type": clean_row[1], "occurred": clean_row[2],
-                        "address": clean_row[3], "status": clean_row[4], "location" : (location.latitude, location.longitude) if location != None else (0, 0)}
-            cleaned_rows.append(row_dict)
+            # location = geolocater.geocode(clean_row[3])
+            report = {"reported": clean_row[0], "type": clean_row[1], "occurred": clean_row[2],
+                        "address": clean_row[3], "status": clean_row[4]}
             row_num += 1
         else:
             clean_row = [x for x in row if len(x) > 0]
-            cleaned_rows[-1]["description"] = clean_row[0]
+            report["description"] = clean_row[0]
+            db.collection(u'crime-logs').add(report)
             row_num += 1
 
-print(cleaned_rows)
