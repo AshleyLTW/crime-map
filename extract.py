440ed4c5e9ab6f864154c42e8d340165ea8cb4d4
Ashley Wong <ashleyltwong@gmail.com>
2019-04-13 13:33:11 -0400

    geo-tagging not working

--- extract.py
+++ extract.py
@@ -2,14 +2,25 @@ import camelot
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from geopy.geocoders import Nominatim
 
 # Set up a firebase instance
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'projectId': project_id,
-})
+cred = credentials.Certificate('./hodp-scraping-f5c8cc1cd113.json')
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+crimes_ref = db.collection(u"crime-logs")
+
+
+report = {"address": "test_address", "description": "nothing happened", "occurred": "1/1/1999 9:41pm",
+            "reported": "13/3/1978 3:50pm", "status": "CLOSED", "type": "test"}
+db.collection(u'crime-logs').add(report)
+
+
+geolocater = Nominatim(user_agent = "harvardopendataproject")
+
+
 
 # Extract and clean data
 tables = camelot.read_pdf("test.pdf", pages = "1-end")
@@ -22,8 +33,10 @@ for table in tables:
     for row in table.data[1:]:
         if row_num % 2 == 0:
             clean_row = [x for x in row if len(x) > 0]
-            row_dict = {"t_reported": clean_row[0], "type": clean_row[1], "t_occurred": clean_row[2],
-                        "location": clean_row[3], "status": clean_row[4]}
+            clean_row[3] = clean_row[3].replace("\n", " ")
+            location = geolocater.geocode(clean_row[3])
+            row_dict = {"reported": clean_row[0], "type": clean_row[1], "occurred": clean_row[2],
+                        "address": clean_row[3], "status": clean_row[4], "location" : (location.latitude, location.longitude) if location != None else (0, 0)}
             cleaned_rows.append(row_dict)
             row_num += 1
         else:
