a701d8221c8a1155fa03d7d9c399ebad1101bf9b
Ashley Wong <ashleyltwong@gmail.com>
2019-04-13 13:58:04 -0400

    convert reported to timestamp

--- extract.py
+++ extract.py
@@ -3,6 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from geopy.geocoders import Nominatim
+import datetime
 
 # Set up a firebase instance
 cred = credentials.Certificate('./hodp-scraping-f5c8cc1cd113.json')
@@ -16,10 +17,7 @@ crimes_ref = db.collection(u"crime-logs")
 
 # Extract and clean data
 tables = camelot.read_pdf("test.pdf", pages = "1-end")
-print(tables)
-
 row_num = 0
-cleaned_rows = []
 
 for table in tables:
     for row in table.data[1:]:
@@ -27,7 +25,8 @@ for table in tables:
             clean_row = [x for x in row if len(x) > 0]
             clean_row[3] = clean_row[3].replace("\n", " ")
             # location = geolocater.geocode(clean_row[3])
-            report = {"reported": clean_row[0], "type": clean_row[1], "occurred": clean_row[2],
+            date_time_obj = datetime.datetime.strptime(clean_row[0], '%m/%d/%y\n%I:%M %p')
+            report = {"reported": date_time_obj, "type": clean_row[1], "occurred": clean_row[2],
                         "address": clean_row[3], "status": clean_row[4]}
             row_num += 1
         else:
