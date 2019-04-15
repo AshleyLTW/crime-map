45e07744c00c9f12913b0b564115bca9075c1418
Ashley Wong <ashleyltwong@gmail.com>
2019-04-14 22:05:21 -0400

    fix 12am syntax issue

--- extract.py
+++ extract.py
@@ -2,7 +2,6 @@ import camelot
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from geopy.geocoders import Nominatim
 import datetime
 from datetime import timedelta
 import re
@@ -12,15 +11,10 @@ cred = credentials.Certificate('./hodp-scraping-f5c8cc1cd113.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
 crimes_ref = db.collection(u"crime-logs")
-
-# geolocater = Nominatim(user_agent = "harvardopendataproject")
 
 #Extract, clean, and insert data into db
 def scrape(url):
-    # tables = camelot.read_pdf(url, pages = "1-end", flavor="stream", edge_tol=200, row_tol = 30)
-    # tables = camelot.read_pdf("test.pdf", pages="1-end")
     tables = camelot.read_pdf(url, pages="1-end")
     row_num = 0
 
@@ -33,9 +27,9 @@ def scrape(url):
                 if re.search(r'\d/\d/\d\d', clean_row[0]):
                     # Account for case where all info is read into one element in a list
                     if len(clean_row) == 1:
-                        # print("unexpected")
                         data = clean_row[0].splitlines()
-                        reported = data[0] +"\n" + data[5]
+                        # Account for when 12am is sometimes written as 0:xx am
+                        reported = (data[0] +"\n" + data[5]).replace("\n0:", "\n12:")
                         occurred = data[1] + "\n" + data[6]
                         address = data[2] + " " + data[7]
                         d_type = data[3]
@@ -45,17 +39,16 @@ def scrape(url):
                                   "address": address, "status": status}
                     # Regular case (each cell read in as separate element)
                     else:
-                        # print("regular")
                         clean_row[3] = clean_row[3].replace("\n", " ")
+                        # Account for when 12am is sometimes written as 0:xx am
+                        clean_row[0] = clean_row[0].replace("\n0:", "\n12:")
                         date_time_obj = datetime.datetime.strptime(clean_row[0], '%m/%d/%y\n%I:%M %p')
                         report = {"reported": date_time_obj, "type": clean_row[1], "occurred": clean_row[2],
                                   "address": clean_row[3], "status": clean_row[4]}
                     row_num += 1
                 else:
-                    # print("anomaly")
                     pass
             else:
-                print("description")
                 clean_row = [x for x in row if len(x) > 0]
                 report["description"] = clean_row[0]
                 # db.collection(u'crime-logs').add(report)
@@ -63,25 +56,25 @@ def scrape(url):
                 row_num += 1
 
 # Find latest date
-# query = crimes_ref.order_by(
-#     u'reported', direction=firestore.Query.DESCENDING).limit(1)
-# results = query.stream()
-# for doc in results:
-#     latest_time = (doc.to_dict ())["reported"]
-#     last_date = datetime.datetime(year=latest_time.year, month=latest_time.month, day=latest_time.day)
-#     last_date += timedelta(days = 1)
-#     cur_date = datetime.datetime.now()
-#     while(last_date < cur_date):
-#         day = str(last_date.day)
-#         month = str(last_date.month)
-#         year = str(last_date.year - 2000)
-#         if(len(month) < 2):
-#             month = "0" + month
-#         if(len(day) < 2):
-#             day = "0" + day
-#         url = "https://www.hupd.harvard.edu/files/hupd/files/" + month + day + year + ".pdf"
-#         # Scrape each url
-#         scrape(url)
-#         last_date += timedelta(days = 1)
+query = crimes_ref.order_by(
+    u'reported', direction=firestore.Query.DESCENDING).limit(1)
+results = query.stream()
+for doc in results:
+    latest_time = (doc.to_dict ())["reported"]
+    last_date = datetime.datetime(year=latest_time.year, month=latest_time.month, day=latest_time.day)
+    last_date += timedelta(days = 1)
+    cur_date = datetime.datetime.now()
+    while(last_date < cur_date):
+        day = str(last_date.day)
+        month = str(last_date.month)
+        year = str(last_date.year - 2000)
+        if(len(month) < 2):
+            month = "0" + month
+        if(len(day) < 2):
+            day = "0" + day
+        url = "https://www.hupd.harvard.edu/files/hupd/files/" + month + day + year + ".pdf"
+        # Scrape each url
+        scrape(url)
+        last_date += timedelta(days = 1)
 
-scrape("https://www.hupd.harvard.edu/files/hupd/files/040619.pdf")
+# scrape("https://www.hupd.harvard.edu/files/hupd/files/040719.pdf")
