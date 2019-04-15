ad027bfacf4f11d84be1de4e3548051d17d6e938
Ashley Wong <ashleyltwong@gmail.com>
2019-04-14 21:55:57 -0400

    fix all cells in one element issue

--- extract.py
+++ extract.py
@@ -1,5 +1,3 @@
-# TODO: Handle the case where it doesn't read through correctly (split on the \n and then split them up and recombine)
-
 import camelot
 import firebase_admin
 from firebase_admin import credentials
@@ -7,9 +5,7 @@ from firebase_admin import firestore
 from geopy.geocoders import Nominatim
 import datetime
 from datetime import timedelta
-from urllib.request import urlopen
 import re
-from matplotlib import pyplot as plt
 
 # Set up a firebase instance
 cred = credentials.Certificate('./hodp-scraping-f5c8cc1cd113.json')
@@ -30,37 +26,58 @@ def scrape(url):
 
     for table in tables:
         for row in table.data[1:]:
+            # Check whether it's info or description
             if row_num % 2 == 0:
                 clean_row = [x for x in row if len(x) > 0]
-                clean_row[3] = clean_row[3].replace("\n", " ")
-                date_time_obj = datetime.datetime.strptime(clean_row[0], '%m/%d/%y\n%I:%M %p')
-                report = {"reported": date_time_obj, "type": clean_row[1], "occurred": clean_row[2],
-                          "address": clean_row[3], "status": clean_row[4]}
-                row_num += 1
+                # Account for the case where description is broken up across two pages
+                if re.search(r'%d/%d/%d%d', clean_row[0]):
+                    # Account for case where all info is read into one element in a list
+                    if len(clean_row) == 1:
+                        data = clean_row[0].splitlines()
+                        reported = data[0] +"\n" + data[5]
+                        occurred = data[1] + "\n" + data[6]
+                        address = data[2] + " " + data[7]
+                        d_type = data[3]
+                        status = data[4]
+                        date_time_obj = datetime.datetime.strptime(reported, '%m/%d/%y\n%I:%M %p')
+                        report = {"reported": date_time_obj, "type": d_type, "occurred": occurred,
+                                  "address": address, "status": status}
+                    # Regular case (each cell read in as separate element)
+                    else:
+                        clean_row[3] = clean_row[3].replace("\n", " ")
+                        date_time_obj = datetime.datetime.strptime(clean_row[0], '%m/%d/%y\n%I:%M %p')
+                        report = {"reported": date_time_obj, "type": clean_row[1], "occurred": clean_row[2],
+                                  "address": clean_row[3], "status": clean_row[4]}
+                    row_num += 1
+                else:
+                    pass
             else:
                 clean_row = [x for x in row if len(x) > 0]
                 report["description"] = clean_row[0]
-                db.collection(u'crime-logs').add(report)
+                # db.collection(u'crime-logs').add(report)
+                print(report)
                 row_num += 1
 
 # Find latest date
-query = crimes_ref.order_by(
-    u'reported', direction=firestore.Query.DESCENDING).limit(1)
-results = query.stream()
-for doc in results:
-    latest_time = (doc.to_dict ())["reported"]
-    last_date = datetime.datetime(year=latest_time.year, month=latest_time.month, day=latest_time.day)
-    last_date += timedelta(days = 1)
-    cur_date = datetime.datetime.now()
-    while(last_date < cur_date):
-        day = str(last_date.day)
-        month = str(last_date.month)
-        year = str(last_date.year - 2000)
-        if(len(month) < 2):
-            month = "0" + month
-        if(len(day) < 2):
-            day = "0" + day
-        url = "https://www.hupd.harvard.edu/files/hupd/files/" + month + day + year + ".pdf"
-        # Scrape each url
-        scrape(url)
-        last_date += timedelta(days = 1)
+# query = crimes_ref.order_by(
+#     u'reported', direction=firestore.Query.DESCENDING).limit(1)
+# results = query.stream()
+# for doc in results:
+#     latest_time = (doc.to_dict ())["reported"]
+#     last_date = datetime.datetime(year=latest_time.year, month=latest_time.month, day=latest_time.day)
+#     last_date += timedelta(days = 1)
+#     cur_date = datetime.datetime.now()
+#     while(last_date < cur_date):
+#         day = str(last_date.day)
+#         month = str(last_date.month)
+#         year = str(last_date.year - 2000)
+#         if(len(month) < 2):
+#             month = "0" + month
+#         if(len(day) < 2):
+#             day = "0" + day
+#         url = "https://www.hupd.harvard.edu/files/hupd/files/" + month + day + year + ".pdf"
+#         # Scrape each url
+#         scrape(url)
+#         last_date += timedelta(days = 1)
+
+scrape("https://www.hupd.harvard.edu/files/hupd/files/040619.pdf")
